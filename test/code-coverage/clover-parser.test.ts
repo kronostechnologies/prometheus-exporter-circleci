3b9104eed1d859c8615172ff441ecce700d6363f
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2019-11-27 12:21:30 -0500

    fixup! Separate coverage parsers files

---
+++ test/code-coverage/clover-parser.test.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { CloverParser } from '../../src/code-coverage/clover-parser';
+
+test('CloverParser parseStream returns CoverageInfo with parsed data', async () => {
+
+    const parser = new CloverParser();
+    const stream = fs.createReadStream(path.join(__dirname, '../../examples/php-test-coverage.xml'));
+    const coverageInfo = await parser.parseStream(stream);
+
+    expect(coverageInfo).toEqual({
+        classes: 36,
+        covered_classes: 22,
+        covered_lines: 243,
+        covered_methods: 34,
+        lines: 4050,
+        methods: 530,
+    });
+});
