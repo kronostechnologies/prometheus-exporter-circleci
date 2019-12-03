a73191b1828b9c5e80cc4d5a74dc96cf03cf54e9
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2019-12-03 11:02:15 -0500

    Strip down code coverage sample files

--- test/code-coverage/clover-parser.test.ts
+++ test/code-coverage/clover-parser.test.ts
@@ -5,12 +5,12 @@ import { CloverParser } from '../../src/code-coverage/clover-parser';
 test('CloverParser parseStream returns CoverageInfo with parsed data', async () => {
 
     const parser = new CloverParser();
-    const stream = fs.createReadStream(path.join(__dirname, '../../examples/php-test-coverage.xml'));
+    const stream = fs.createReadStream(path.join(__dirname, '../../examples/php-test-coverage.clover.xml'));
     const coverageInfo = await parser.parseStream(stream);
 
     expect(coverageInfo).toEqual({
         classes: 36,
-        covered_classes: 22,
+        covered_classes: 1,
         covered_lines: 243,
         covered_methods: 34,
         lines: 4050,
--- test/code-coverage/jacoco-parser.test.ts
+++ test/code-coverage/jacoco-parser.test.ts
@@ -5,7 +5,7 @@ import { JacocoParser } from '../../src/code-coverage/jacoco-parser';
 test('JacocoParser parseStream returns CoverageInfo with parsed data', async () => {
 
     const parser = new JacocoParser();
-    const stream = fs.createReadStream(path.join(__dirname, '../../examples/unit-test-coverage.xml'));
+    const stream = fs.createReadStream(path.join(__dirname, '../../examples/kotlin-test-coverage.jacoco.xml'));
     const coverageInfo = await parser.parseStream(stream);
 
     expect(coverageInfo).toEqual({
