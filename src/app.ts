31ed8d7dd02cdc98ac2e4997a7ea92c8614037d2
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2020-03-31 10:58:05 -0400

    New /healthcheck route and better handling of sigterm

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
+import { createTerminus, HealthCheckError, TerminusOptions } from '@godaddy/terminus';
 import express from 'express';
-import { Server } from 'http';
+import { createServer, Server } from 'http';
 import Prometheus from 'prom-client';
 import { CircleCiClient } from './circleci-client';
 import { CircleCiExporter } from './circleci-exporter';
 import appConfig from './config';
 import logger from './logger';
 import { ScrapeStatus } from './scrape-status';
-import Signals = NodeJS.Signals;
 
 // Debug environment configuration
 logger.silly('Environment variables');
@@ -14,17 +14,50 @@ logger.silly(process.env);
 logger.silly('Configuration');
 logger.silly(appConfig);
 
-let server: Server | undefined;
-
 const pagingOptions = {
     limit: appConfig.scrape.buildsPerPage,
     maxPages: appConfig.scrape.maxPages,
     since: appConfig.scrape.since,
 };
+
 const exporter = new CircleCiExporter(new CircleCiClient(appConfig.circleci), new ScrapeStatus());
+const app = express();
+const server: Server = createServer(app);
+
+let initialScrapingFinished = false;
+
+async function healthCheck(): Promise<any> {
+    if (!initialScrapingFinished) {
+        throw new HealthCheckError('healthcheck failed', ['Initial scraping not finished']);
+    }
+    return {};
+}
+
+async function onSignal(): Promise<any> {
+    logger.info('caught signal. Starting cleanup');
+}
+
+async function onShutdown(): Promise<any> {
+    logger.info('cleanup finished, server is shutting down');
+}
+
+const terminusOptions: TerminusOptions = {
+    healthChecks: {
+        '/healthcheck': healthCheck,
+    },
+    signals: [ 'SIGINT', 'SIGTERM' ],
+    onSignal,
+    onShutdown,
+    logger: logger.info,
+};
+
+createTerminus(server, terminusOptions);
+
+server.listen(appConfig.http.port, async () => {
+    logger.info(`App server listening on port ${appConfig.http.port}!`);
+});
+
 exporter.export(pagingOptions).then(() => {
-    // Start the server only after the first metrics collection is done
-    const app = express();
     app.get('/metrics', async (_, res) => {
         logger.debug('/metrics hit');
 
@@ -37,31 +70,5 @@ exporter.export(pagingOptions).then(() => {
         res.set('Content-Type', Prometheus.register.contentType);
         res.end(Prometheus.register.metrics());
     });
-
-    server = app.listen(appConfig.http.port, async () => {
-        logger.info(`App server listening on port ${appConfig.http.port}!`);
-    });
+    initialScrapingFinished = true;
 }).catch(logger.error);
-
-// @ts-ignore
-function doScrape(): void {
-    exporter.export(pagingOptions).then(() => {
-        setTimeout(doScrape, 60_000);
-    }).catch(logger.error);
-}
-
-(['SIGINT', 'SIGTERM'] as Signals[]).forEach(signal => {
-    process.on(signal, () => {
-        logger.info(`Received signal ${signal}'`);
-        server && server.close(err => {
-            logger.info('Server is stopping');
-            err && logger.error(err);
-            process.exit(0);
-        });
-        // Make sure we forcefully stop the server even if server is still responding
-        setTimeout(() => {
-            logger.warn('Could not close connections in time, forcefully shutting down');
-            process.exit(0);
-        }, 2000);
-    });
-});
